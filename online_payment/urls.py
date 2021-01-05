8b69307948f3d47eb87478ef6f68a6f1ea012e75
Tushar-Faroque <tahrim.tushar@northsouth.edu>
2021-01-06 00:24:19 +0600

    added payment, home, login, register html backbone

--- online_payment/urls.py
+++ online_payment/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.test)
+    path('home', views.home),
+    path('login', views.login),
+    path('register', views.register),
+    path('payment', views.payment),
+    
 ]
--- online_payment/views.py
+++ online_payment/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def test(request):
-    return HttpResponse('I am Here')
+def home(request):
+    return render(request, 'online_payment/index.html')
+
+def login(request):
+    return render(request, 'online_payment/login.html')
+
+def register(request):
+    return render(request, 'online_payment/registration.html')
+
+def payment(request):
+    return render(request, 'online_payment/payment.html')
