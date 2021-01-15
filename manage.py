8f10796379fbc749df38cd08a065b5cb088b430d
Mim-Zohura <fatima.zohura@northsouth.edu>
2021-01-16 00:02:28 +0600

    fixing merge conflict

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GetBloodOrgan.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_blood_and_organ_bank.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- online_blood_and_organ_bank/settings.py
+++ online_blood_and_organ_bank/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'GetBloodOrgan.urls'
+ROOT_URLCONF = 'online_blood_and_organ_bank.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'GetBloodOrgan.wsgi.application'
+WSGI_APPLICATION = 'online_blood_and_organ_bank.wsgi.application'
 
 
 # Database
